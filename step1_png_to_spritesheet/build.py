e4e2c4f30e26f1689149939613545b6dc36be2e8
jalagar <galagar.eth.88@gmail.com>
2022-03-28 09:49:23 -0400

    Update logic to read from global config and update build

--- step1_png_to_spritesheet/build.py
+++ step1_png_to_spritesheet/build.py
@@ -1,15 +1,14 @@
 from PIL import Image as PIL_Image
 import os
 from PIL.Image import Image
-from typing import List
+from typing import List, Tuple
 import json
 from file import get_png_file_name, setup_directory
 
+
 layers_directory = "./layers"
 output_directory = "./step1_png_to_spritesheet/output"
-
-# TODO read this from a config
-NUM_IMAGES = 12
+json_config = "./config.json"
 
 
 def combine_images(images: List[Image]) -> Image:
@@ -31,21 +30,68 @@ def combine_images(images: List[Image]) -> Image:
     return dst
 
 
-def sort_function(file: str):
+def sort_function(file: str) -> int:
     """
     Sorts based on the integer file name ex. 2.py.
-    That way 2.py comes before 10.py. Ignores
-    other file names if it is not a valid integer.
+    That way 2.py comes before 10.py. Returns
+    0 for any invalid file name.
+
+    :param file: filename
+    :returns: int
     """
     try:
         return int(get_png_file_name(file))
     except:
-        return file
+        if file != "config.json":
+            print(f"Invalid integer layer, ordering may not be accurage for", file)
+        return 0
+
+
+def parse_global_config() -> dict:
+    global_config_file = open(json_config, "r")
+    global_config_json = json.load(global_config_file)
+    global_config_file.close()
+    return global_config_json
+
+
+def parse_attributes_into_images(
+    attribute_path: str, num_frames: int, is_debug: bool
+) -> Tuple[List[Image], int]:
+    images = []
+
+    rarity_percentage = None
+    for filename in sorted(os.listdir(attribute_path), key=sort_function):
+        file_path = os.path.join(attribute_path, filename)
+        if filename == "config.json":
+            config_file = open(file_path, "r")
+            config_json = json.load(config_file)
+            config_file.close()
+            rarity_percentage = int(config_json["rarity"])
+        if filename.endswith(".png"):
+            img = PIL_Image.open(file_path)
+            images.append(img)
+
+    if rarity_percentage is None:
+        if is_debug:
+            print(
+                f"Missing rarity percentage for attribute: {attribute_path}, defaulting to 100%"
+            )
+        rarity_percentage = 100
+
+    if len(images) == 1:
+        if is_debug:
+            print(
+                f"Only one image found for: {attribute_path}, duplicating images {num_frames} number of times"
+            )
+        images = images * num_frames
+    return images, rarity_percentage
 
 
 def main():
     print("********Starting step 1: Converting pngs to spritesheets********")
-    for layer_folder in sorted(os.listdir(layers_directory), key=sort_function):
+    global_config_json = parse_global_config()
+    is_debug = global_config_json["debug"]
+    for layer_folder in os.listdir(layers_directory):
         layer_path = os.path.join(layers_directory, layer_folder)
         output_path = os.path.join(output_directory, layer_folder)
         setup_directory(output_path)
@@ -56,24 +102,14 @@ def main():
                 attribute_path = os.path.join(layer_path, attribute_folder)
                 if os.path.isdir(attribute_path):
                     print(f"Parsing attributes in folder: {attribute_folder}")
-                    images = []
-                    # Default 100% rarity which would give them equal weight
-                    rarity_percentage = 100
-                    for filename in sorted(os.listdir(attribute_path)):
-                        file_path = os.path.join(attribute_path, filename)
-                        if filename == "config.json":
-                            config_file = open(file_path, "r")
-                            config_json = json.load(config_file)
-                            rarity_percentage = config_json["rarity"]
-                        if filename.endswith(".png"):
-                            img = PIL_Image.open(file_path)
-                            images.append(img)
 
-                    if len(images) == 1:
-                        images = images * NUM_IMAGES
-
-                    dst = combine_images(images)
-                    dst.save(
+                    images, rarity_percentage = parse_attributes_into_images(
+                        attribute_path,
+                        num_frames=global_config_json["numberOfFrames"],
+                        is_debug=is_debug,
+                    )
+                    spritesheet = combine_images(images)
+                    spritesheet.save(
                         os.path.join(
                             output_path, f"{attribute_folder}#{rarity_percentage}.png"
                         )
--- step3_generative_sheet_to_gif/build.py
+++ step3_generative_sheet_to_gif/build.py
@@ -1,18 +1,31 @@
 from PIL import Image
 import os
-
+import json
 from file import get_png_file_name, setup_directory
 
 output_directory = "./build/gifs"
 input_directory = "./step2_spritesheet_to_generative_sheet/output/images"
 temp_directory = "./step3_generative_sheet_to_gif/temp"
+json_config = "./config.json"
 
 
-def crop(file_name, height, width, area=None):
+def parse_global_config() -> dict:
+    global_config_file = open(json_config, "r")
+    global_config_json = json.load(global_config_file)
+    global_config_file.close()
+    return global_config_json
+
+
+def crop_and_save(file_name: str, height: int, width: int) -> None:
+    """
+    Crops image into squares and saves them into temp folder
+
+    :param file_name: name of file
+    :param height: height of each frame
+    :param width: width of each frame
+    :returns: int - duration for each frame in milliseconds
+    """
     file = os.path.join(input_directory, file_name)
-    if not area:
-        area = height * width
-
     im = Image.open(file)
     k = 0
     imgwidth, imgheight = im.size
@@ -30,7 +43,7 @@ def crop(file_name, height, width, area=None):
             k += 1
 
 
-def convert_pngs_to_gif(file_name: str):
+def convert_pngs_to_gif(file_name: str, quality: int, duration: int):
     frames = []
 
     temp_img_folder = os.path.join(temp_directory, get_png_file_name(file_name))
@@ -48,14 +61,38 @@ def convert_pngs_to_gif(file_name: str):
         format="GIF",
         append_images=frames[1:],
         save_all=True,
-        duration=125,
+        duration=duration,
         loop=0,
-        quality=95,
+        quality=quality,
     )
+
+
+def fps_to_ms_duration(fps: int) -> int:
+    """
+    Converts frames per second to millisecond duration.
+    PIL library takes in millisecond duration per frame
+    which is unintuitive from an animation perspective.
+
+    NOTE - this will not always be exact given the library
+    takes in an integer for number of milliseconds.
+    Ex. 12fps = 83.33ms per frame, but the code will convert
+    it to 83. Not noticeable by the human eye but still
+    worth calling out.
+
+    :param file: fps - frames per second
+    :returns: int - duration for each frame in milliseconds
+    """
+    return int(1000 / fps)
 
 
 def main():
     print("Starting step 3: Converting sprite sheets to gifs")
+    global_config = parse_global_config()
+    quality = global_config["quality"]
+    fps = global_config["framesPerSecond"]
+    height = global_config["height"]
+    width = global_config["width"]
+
     for folder in [output_directory, temp_directory]:
         setup_directory(folder)
 
@@ -64,12 +101,12 @@ def main():
     ):
         print(f"Converting spritesheet to gif for {filename}")
         if filename.endswith(".png"):
-            crop(
+            crop_and_save(
                 filename,
-                1000,
-                1000,
+                height,
+                width,
             )
-            convert_pngs_to_gif(filename)
+            convert_pngs_to_gif(filename, quality, fps_to_ms_duration(fps))
 
 
 if __name__ == "__main__":
