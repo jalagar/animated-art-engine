8d1af5423620d6935624014c4a0fddf5a8f6d30c
Jalagar <98709501+jalagar@users.noreply.github.com>
2022-04-15 08:29:03 -0400

    Support gif layers instead of png frames (#29)
    * Add support for gif layers
    
    * Add readme and pre-commit hooks

--- step1_layers_to_spritesheet/build.py
+++ step1_layers_to_spritesheet/build.py
@@ -12,9 +12,11 @@ from utils.file import setup_directory, sort_function, parse_global_config
 
 global_config_json = parse_global_config()
 is_debug = global_config_json["debug"]
+num_frames = global_config_json["numberOfFrames"]
 
-layers_directory = f"./{global_config_json['layersFolder']}"
-output_directory = "./step1_layers_to_spritesheet/output"
+LAYERS_DIRECTORY = f"./{global_config_json['layersFolder']}"
+TEMP_DIRECTORY = "./step1_layers_to_spritesheet/temp"
+OUTPUT_DIRECTORY = "./step1_layers_to_spritesheet/output"
 
 
 def combine_images(images: List[Image]) -> Image:
@@ -60,7 +62,7 @@ def duplicate_images_number_of_frames_times(images: List[Image], num_frames: int
 
 
 def parse_attributes_into_images(
-    attribute_folder: str, attribute_path: str, num_frames: int, output_path: bool
+    attribute_folder: str, attribute_path: str, output_path: bool
 ) -> Tuple[List[Image], bool]:
     """
     Mutual recursive function that parses the attributes
@@ -71,7 +73,6 @@ def parse_attributes_into_images(
 
     :param attribute_folder: Folder name of attribute
     :param attribute_path: Path to folder of attribute
-    :param num_frames: number of total frames in the list
     :param output_path: output path to save images
     :returns: Tuple of list of images, and a boolean indicating if there contains a subfolder
     """
@@ -119,7 +120,6 @@ def parse_attribute_folders(
     images, containsSubFolder = parse_attributes_into_images(
         attribute_folder,
         attribute_path,
-        num_frames=global_config_json["numberOfFrames"],
         output_path=output_path,
     )
     if len(images) == 0:
@@ -132,19 +132,73 @@ def parse_attribute_folders(
         output_folder = os.path.join(output_path, attribute_folder)
         if not os.path.exists(output_folder):
             setup_directory(output_folder)
-        spritesheet.save(os.path.join(output_folder, f"{attribute_folder}.png"))
+        spritesheet.save(
+            os.path.join(output_folder, f"{attribute_folder}.png"), quality=95
+        )
     else:
-        spritesheet.save(os.path.join(output_path, f"{attribute_folder}.png"))
+        spritesheet.save(
+            os.path.join(output_path, f"{attribute_folder}.png"), quality=95
+        )
+
+
+def search_folders_for_gifs(directory: str) -> bool:
+    """
+    Recursive function that searches folders for gifs
+    :param directory: File path to directory
+    :returns: bool - True or False if a gif exists
+    """
+    found_gif = False
+    for filename in os.listdir(directory):
+        file_path = os.path.join(directory, filename)
+        if filename.endswith("gif"):
+            found_gif = True
+        if os.path.isdir(file_path):
+            found_gif = search_folders_for_gifs(file_path) or found_gif
+    return found_gif
+
+
+def parse_gifs_into_temp_directory(directory: str, output_directory: str) -> None:
+    """
+    Parses gifs in the layers folder and splits them into output directory
+    :param directory - file path to layers directory
+    :param output_directory - file path to output_directory
+    :returns: None
+    """
+    for filename in os.listdir(directory):
+        file_path = os.path.join(directory, filename)
+        output_path = os.path.join(output_directory, filename)
+        if filename.endswith("gif"):
+            pil_gif = PIL_Image.open(file_path)
+            try:
+                for i in range(num_frames):
+                    pil_gif.seek(i)
+                    pil_gif.save(os.path.join(output_directory, f"{i}.png"), quality=95)
+            except EOFError:
+                # Ran out of frames, not all gifs need to be the same length
+                break
+
+        if filename.endswith("png"):
+            shutil.copyfile(file_path, output_path)
+        if os.path.isdir(file_path):
+            setup_directory(output_path, delete_if_exists=False)
+            parse_gifs_into_temp_directory(file_path, output_path)
 
 
 def main():
     print("********Starting step 1: Converting pngs to spritesheets********")
 
-    setup_directory(output_directory)
+    setup_directory(OUTPUT_DIRECTORY)
+    contains_gif_layer = search_folders_for_gifs(LAYERS_DIRECTORY)
+    if contains_gif_layer:
+        setup_directory(TEMP_DIRECTORY)
+        parse_gifs_into_temp_directory(LAYERS_DIRECTORY, TEMP_DIRECTORY)
+        layers_directory = TEMP_DIRECTORY
+    else:
+        layers_directory = LAYERS_DIRECTORY
     for layer_folder in os.listdir(layers_directory):
         layer_path = os.path.join(layers_directory, layer_folder)
 
-        output_layer_path = os.path.join(output_directory, layer_folder)
+        output_layer_path = os.path.join(OUTPUT_DIRECTORY, layer_folder)
         # hidden files should be ignored
         if layer_folder.startswith("."):
             continue
