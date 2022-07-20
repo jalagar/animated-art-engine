bb6fb7a50550702a13ebb61c89f39f8f5f731dc5
Neonode22 <108744991+Neonode22@users.noreply.github.com>
2022-07-20 11:15:54 +0800

    Update solanaConfig.js

--- step2_spritesheet_to_generative_sheet/Solana/solanaConfig.js
+++ step2_spritesheet_to_generative_sheet/Solana/solanaConfig.js
@@ -10,16 +10,16 @@
  * Credits:
  * Metaplex.js util by https://github.com/DawidAbram
  */
-const NFTName = "NameOfNFT" //This is the name there will be showen on your NFTs !!! Name can at max be 32 characters !!!
-const collectionName = "PROJECT_NAME"; //This is used if mutiple collection is needed
-const collectionFamily = "PROJECT_FAMILY"; // Many projects can belong to one family
-const symbol = "PRJSMBL"; // !!! Symbol can at max be 10 characters !!!
+const NFTName = "" //This is the name there will be showen on your NFTs !!! Name can at max be 32 characters !!!
+const collectionName = ""; //This is used if mutiple collection is needed
+const collectionFamily = "false"; // Many projects can belong to one family
+const symbol = ""; // !!! Symbol can at max be 10 characters !!!
 
 const baseUriPrefix = ""; // OPTIONAL, if you need to prefix your image#.png with a baseURI
-const description = "Default Solana Description";
+const description = ""; //collection/image description
 const external_url = ""; // add optional external URL here, e.g, https://0n10nDivision.com
 
-const royaltyFee = 200; // This is 2% royalty fee
+const royaltyFee = 400; // This is 2% royalty fee
 
 /**
  * Array of Creators.
@@ -27,7 +27,7 @@ const royaltyFee = 200; // This is 2% royalty fee
  */
 const creators = [
     {
-        address: "WALLET_ADDRESS", // Wallet address for royalties
+        address: "", // Wallet address for royalties
         share: 100, // Amount of shares for this wallet, can be more than one, all have to add up to 100 together !!! And a maximum of 4 creators !!!
     },
     // uncomment and edit for additional creator.
