cf9df3988dac516f5a9d6b669c746d97fe7b33c0
Jalagar <galagar.eth.88@gmail.com>
2022-09-25 16:56:16 +0700

    Add support for html page toggle animation

---
+++ html_generate.py
@@ -0,0 +1,16 @@
+import base64
+
+
+with open('./build/pfp/0.png', 'rb') as binary_file:
+    binary_file_data = binary_file.read()
+    base64_encoded_data = base64.b64encode(binary_file_data)
+    base64_message = base64_encoded_data.decode('utf-8')
+
+    print(base64_message)
+
+with open('./build/gif/0.gif', 'rb') as binary_file:
+    binary_file_data = binary_file.read()
+    base64_encoded_data = base64.b64encode(binary_file_data)
+    base64_message = base64_encoded_data.decode('utf-8')
+
+    print(base64_message)
